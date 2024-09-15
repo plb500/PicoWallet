4dad3339dc5b9b138c98f1614de838aeed3998ca
Philip Bull <plb500@gmail.com>
2024-09-15 02:59:58 -0700

    Generate QR for both private and public addresses/keys

--- pico/src/pico_wallet.c
+++ pico/src/pico_wallet.c
@@ -9,6 +9,92 @@ const char* KEY_HEADER                  = "<#8BitVault_OK!>";
 #define KEY_HEADER_SIZE     (16)
 
 
+void print_qr_addresses(const uint8_t* privateAddress, const uint8_t* publicAddress) {
+    // The structure to manage the QR code
+    QRCode qrcodePrivate;
+    QRCode qrcodePublic;
+
+    // Allocate a chunk of memory to store the QR codes
+    uint8_t qrcodeBytesPrivate[qrcode_getBufferSize(3)];
+    uint8_t qrcodeBytesPublic[qrcode_getBufferSize(3)];
+    
+   qrcode_initText(&qrcodePrivate, qrcodeBytesPrivate, 3, ECC_LOW, privateAddress);
+   qrcode_initText(&qrcodePublic, qrcodeBytesPublic, 3, ECC_LOW, publicAddress);
+
+    // Header row
+    printf("\u2588\u2588\u2588\u2588");
+    for (uint8_t x = 0; x < qrcodePrivate.size; x++) {
+        printf("\u2588\u2588");
+    }
+    printf("\u2588\u2588\u2588\u2588    \u2588\u2588\u2588\u2588");
+    for (uint8_t x = 0; x < qrcodePublic.size; x++) {
+        printf("\u2588\u2588");
+    }
+    printf("\u2588\u2588\u2588\u2588\n");
+
+    // Top gap row
+    printf("\u2588\u2588  ");
+    for (uint8_t x = 0; x < qrcodePrivate.size; x++) {
+        printf("  ");
+    }
+    printf("  \u2588\u2588    \u2588\u2588  ");
+    for (uint8_t x = 0; x < qrcodePublic.size; x++) {
+        printf("  ");
+    }
+    printf("  \u2588\u2588\n");
+
+    // QR code rows
+    for (uint8_t y = 0; y < qrcodePrivate.size; y++) {
+
+        printf("\u2588\u2588  ");
+        
+        // Private key
+        for (uint8_t x = 0; x < qrcodePrivate.size; x++) {
+            if (qrcode_getModule(&qrcodePrivate, x, y)) {
+                printf("\u2588\u2588");
+            } else {
+                printf("  ");
+            }
+        }
+
+        printf("  \u2588\u2588    \u2588\u2588  ");
+
+        // Public
+        for (uint8_t x = 0; x < qrcodePublic.size; x++) {
+            if (qrcode_getModule(&qrcodePublic, x, y)) {
+                printf("\u2588\u2588");
+            } else {
+                printf("  ");
+            }
+        }
+
+        printf("  \u2588\u2588");
+        printf("\n");
+    }
+
+    // Bottom gap row
+    printf("\u2588\u2588  ");
+    for (uint8_t x = 0; x < qrcodePrivate.size; x++) {
+        printf("  ");
+    }
+    printf("  \u2588\u2588    \u2588\u2588  ");
+    for (uint8_t x = 0; x < qrcodePublic.size; x++) {
+        printf("  ");
+    }
+    printf("  \u2588\u2588\n");
+
+    // Footer row
+    printf("\u2588\u2588\u2588\u2588");
+    for (uint8_t x = 0; x < qrcodePrivate.size; x++) {
+        printf("\u2588\u2588");
+    }
+    printf("\u2588\u2588\u2588\u2588    \u2588\u2588\u2588\u2588");
+    for (uint8_t x = 0; x < qrcodePublic.size; x++) {
+        printf("\u2588\u2588");
+    }
+    printf("\u2588\u2588\u2588\u2588\n");
+}
+
 void print_qr_code(const uint8_t* address) {
     // The structure to manage the QR code
     QRCode qrcode;
@@ -59,7 +145,8 @@ void print_qr_code(const uint8_t* address) {
 
 void debug_key(const char* title, const ExtendedKey* key) {
     int addressLen;
-    uint8_t address[128];
+    uint8_t privateAddress[128];
+    uint8_t publicAddress[64];
 
     printf("Extended key - %s\n\n", title);
 
@@ -79,43 +166,7 @@ void debug_key(const char* title, const ExtendedKey* key) {
     for(int i = 0; i < PUBLIC_KEY_LENGTH; ++i) {
         printf("%02X", key->publicKey[i]);
     }
-    printf("\n\n");
-
-    printf("   Private address: ");
-    addressLen = get_extended_private_key_address(key, address);
-    for(int i = 0; i < addressLen; ++i) {
-        printf("%c", address[i]);
-    }
     printf("\n");
-    printf("         +     WIF: ");
-    addressLen = get_private_key_wif(key, BTC_MAIN_NET, address);
-    printf("%s", address);
-    printf("\n");
-    
-    printf("         + QR Code: \n\n");
-    print_qr_code(address);
-    printf("\n\n");
-
-    printf("    Public address: ");
-    addressLen = get_extended_public_key_address(key, address);
-    for(int i = 0; i < addressLen; ++i) {
-        printf("%c", address[i]);
-    }
-    printf("\n");
-    printf("          +  P2PKH: ");
-    addressLen = get_p2pkh_public_address(key, address);
-    for(int i = 0; i < addressLen; ++i) {
-        printf("%c", address[i]);
-    }
-    printf("\n");
-
-    printf("          + P2WPKH: ");
-    addressLen = get_p2wpkh_public_address(key, address);
-    for(int i = 0; i < addressLen; ++i) {
-        printf("%c", address[i]);
-    }
-    printf("\n\n");
-
 
     printf("       Fingerprint: ");
     for(int i = 0; i < FINGERPRINT_LENGTH; ++i) {
@@ -129,8 +180,49 @@ void debug_key(const char* title, const ExtendedKey* key) {
     }
     printf("\n");
 
-    printf("             Index: 0x%08X\n", key->index);
-    printf("\n            ************\n\n");
+    printf("             Index: 0x%08X\n\n", key->index);
+
+
+    printf("   Private address: ");
+    addressLen = get_extended_private_key_address(key, privateAddress);
+    for(int i = 0; i < addressLen; ++i) {
+        printf("%c", privateAddress[i]);
+    }
+    printf("\n");
+    printf("         +     WIF: ");
+    addressLen = get_private_key_wif(key, BTC_MAIN_NET, privateAddress);
+    printf("%s", privateAddress);
+    printf("\n");
+    
+    printf("    Public address: ");
+    addressLen = get_extended_public_key_address(key, privateAddress);
+    for(int i = 0; i < addressLen; ++i) {
+        printf("%c", privateAddress[i]);
+    }
+    printf("\n");
+    printf("          +  P2PKH: ");
+    addressLen = get_p2pkh_public_address(key, publicAddress);
+    for(int i = 0; i < addressLen; ++i) {
+        printf("%c", publicAddress[i]);
+    }
+    printf("\n");
+
+    printf("          + P2WPKH: ");
+    addressLen = get_p2wpkh_public_address(key, publicAddress);
+    for(int i = 0; i < addressLen; ++i) {
+        printf("%c", publicAddress[i]);
+    }
+    printf("\n\n");
+
+    printf("                   ************\n");
+    printf("                   * QR Codes *\n");
+    printf("                   ************\n\n");
+    get_private_key_wif(key, BTC_MAIN_NET, privateAddress);
+    get_p2pkh_public_address(key, publicAddress);
+    print_qr_addresses(privateAddress, publicAddress);
+
+
+    printf("\n\n\n");
 }
 
 
